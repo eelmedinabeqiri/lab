920700a88ff79040e4090d9375bfae05ce4bf662
LoretaZeka <lz45398@ubt-uni.net>
2020-08-01 11:32:30 +0200

    admin added

---
+++ SignUp.php
@@ -0,0 +1,118 @@
+
+
+REGISTER.HTML
+<html>
+    <link href="css/register.css" rel="stylesheet" >
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"  ></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <body>
+        <header>
+            <nav>
+    <ul class="nav_links">
+        <li><a>Checkout</a</li>
+         <li><a>Wishlist  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a> </li>
+        <li><a>Account &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a</li>
+       
+             </ul>
+            <img class="logo"src="LogoMakr_7lK4DC.png">
+            </nav>
+           
+
+            <div class="menu">
+                <div class="menu-1">
+            <div class="menu-2"> 
+                <li><a>Home</a></li>
+            </div>
+                <div class="menu-3">
+                <li><a>Books</a></li>
+                </div>
+                <div class="menu-4">
+                <li><a>LogIn</a></li>
+            </div>
+            <div class="menu-5">
+                <li><a>Shop</a></li>
+            </div>
+           
+            </div>
+            <div class="search-box">
+                <input type="text" placeholder="Type to search">
+            </div>
+            </div>
+        </header>
+        <div class="containerss">
+            <p> <br>
+             SIGN IN
+            <br>
+       </div>
+      
+              <div class="bg-image">
+                  
+ 
+<div class="content">
+    <div class="form">
+        <form onsubmit= "return validate()" action="db/SignUpdb.php" id="form" method="POST">
+        <label for="">name: </label><br>
+        <input type="text" name="name" id="name" required ><br>
+        <label for="">surname: </label><br>
+        <input type="text" name="surname" id="surname" required ><br>
+        <label for="">email: </label><br>
+        <input type="email" name="email" id="email" required><br>
+        <label for="">username: </label><br>
+        <input type="text" name="username" id="username" required><br>
+        <label for="">password: </label><br>
+        <input type="Password" name="password"  id="password" required><br>
+        <input type="Submit" name="submit" value="Sign Up" id="Submit" required>
+        <li><a href="LogIn.php">Already have an account? Log in</a></li>
+    </form>
+</div>
+              </div>
+      
+          </div>
+          <div class="fotter">
+            <img class="logoo"src="LogoMakr_7lK4DC.png">
+            <div class="Fotter1">
+                <li><a>Kosov</a></li>
+            </div>
+            <div class="Fotter1">
+                <li><a>+38349444666</a></li>
+            </div>
+            <div class="Fotter1">
+                <li><a>Bokforyou@homtail.com</a></li>
+            </div>
+            
+            <div class=Fotter2>
+                
+                    <li><a>Costumer Servises</a></li>
+                </div>
+                <div class="Fotter2">
+                    <li><a>About Us</a></li>
+                </div>
+                <div class="Fotter2">
+                    <li><a>Delivery Information</a></li>
+                </div>
+                <div class="Fotter4">
+                    <li><a>Information</a></li>
+                </div>
+                <div class="map">
+                
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2947.7852850919016!2d21.152347914744677!3d42.3684149791863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13547e5844c64dcb%3A0x904efc11559725f4!2sUniversiteti%20%22UBT%22%20Ferizaj!5e0!3m2!1sen!2s!4v1595863884474!5m2!1sen!2s" width="400" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+        <div class="social-media">
+            
+            <img class="foto4"src="libra1.jpg" alt="foto4">
+            <img class="foto4"src="libra2.jpg" alt="foto4">
+            <img class="foto4"src="libra3.jpg" alt="foto4">
+
+        </div>
+            </div>
+
+     
+        </div>
+        
+        
+
+
+      
+   </body>
+   </html>
+      
